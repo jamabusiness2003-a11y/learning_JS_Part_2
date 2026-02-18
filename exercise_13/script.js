13513c7ebb118bed91038980738a0367fd441cc0
Jamario <jamariomerzon@gmail.com>
2026-02-17 19:25:49 -0500

    Added Audio and the rest of the features

--- exercise_13/script.js
+++ exercise_13/script.js
@@ -4,15 +4,18 @@ const secondsInput = document.getElementById("seconds");
 const startBtn = document.getElementById("startBtn");
 const pauseBtn = document.getElementById("pauseBtn");
 const resetBtn = document.getElementById("resetBtn");
+const audioPlayer = document.getElementById("audioPlayer");
 
 const maxTime = 60;
 
 let currentMinutes = 0;
 let currentSeconds = 0;
 
-let intervalId;
+let intervalId = null;
 
-startBtn.addEventListener("click", startTimer, {once: true});
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resetBtn.addEventListener("click", resetTimer);
 
 function renderTimer() {
     const minutes = currentMinutes.toString().padStart(2, 0);
@@ -22,15 +25,34 @@ function renderTimer() {
 }
 
 function startTimer() {
+    if (minutesInput.value === "0" && secondsInput.value === "0") return;
+
+    if (intervalId) return;
+
     currentMinutes = parseInt(minutesInput.value);
     currentSeconds = parseInt(secondsInput.value);
 
     intervalId = setInterval(() => {
         const previousSeconds = currentSeconds;
+        const previousMinutes = currentMinutes;
+
         currentSeconds = (currentSeconds - 1 + maxTime) % maxTime;
 
         if (currentSeconds === maxTime - 1 && previousSeconds === 0) {
             currentMinutes--;
+        }
+
+        if (currentMinutes <= 2 && currentSeconds === 0) {
+            displayTimer.classList.add("low-time");
+        } else {
+            displayTimer.classList.remove("low-time");
+        }
+
+        if (previousSeconds <= 0 && previousMinutes <= 0) {
+            clearInterval(intervalId);
+            audioPlayer.play();
+            alert("Countdown Finished.");
+            return;
         }
         renderTimer();
         
@@ -41,5 +63,15 @@ function startTimer() {
 
 function pauseTimer() {
     clearInterval(intervalId);
+    intervalId = null;
     renderTimer();
 }
+
+function resetTimer() {
+    clearInterval(intervalId);
+    intervalId = null;
+    currentMinutes = 0;
+    currentSeconds = 0;
+    renderTimer();
+}
+
