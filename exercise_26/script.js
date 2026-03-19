a3554ba34e0b69a2ae6e4181f8897646a78ca420
Jamario <jamariomerzon@gmail.com>
2026-03-19 19:59:05 -0400

    Finished Exercise 26

---
+++ exercise_26/script.js
@@ -0,0 +1,40 @@
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const resetBtn = document.getElementById("resetBtn");
+const statusDisplay = document.getElementById("statusDisplay");
+
+const shortcuts = {
+    "Ctrl+c": () => displayStatus("Start"), 
+    "Ctrl+v": () => displayStatus("Stop"), 
+    "Ctrl+b": () => displayStatus("Idle")
+};
+
+startBtn.addEventListener("click", shortcuts["Ctrl+c"]);
+stopBtn.addEventListener("click", shortcuts["Ctrl+v"]);
+resetBtn.addEventListener("click", shortcuts["Ctrl+b"]);
+
+
+document.addEventListener("keydown", (e) => {
+    const combo = normalizeKey(e);
+    
+    if (shortcuts[combo]){
+        e.preventDefault();
+        shortcuts[combo]();
+    }
+});
+
+function displayStatus(status) {
+    statusDisplay.classList.remove("idle", "start", "stop");
+
+    statusDisplay.textContent = `Status: ${status}`;
+    statusDisplay.classList.add(status.toLowerCase());
+}
+
+function normalizeKey(e) {
+    let keys = [];
+
+    if (e.ctrlKey) keys.push("Ctrl");
+
+    keys.push(e.key.toLowerCase());
+    return keys.join("+");
+}
